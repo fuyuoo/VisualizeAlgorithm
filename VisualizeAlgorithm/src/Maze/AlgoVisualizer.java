d0b2dff005f111bc07cc76e25fe5e2e31d1609cb
eren <574246399@qq.com>
2019-11-27 14:19:00 +0800

    dfs not recursion

--- VisualizeAlgorithm/src/Maze/AlgoVisualizer.java
+++ VisualizeAlgorithm/src/Maze/AlgoVisualizer.java
@@ -4,6 +4,7 @@ import sort.heap.HeapSortData;
 import util.AlgoVisuHelper;
 
 import java.awt.*;
+import java.util.Stack;
 
 public class AlgoVisualizer {
     private MazeData data;
@@ -11,9 +12,9 @@ public class AlgoVisualizer {
     private int sceneWidth;
     private int sceneHeight;
     private static int blockSide = 8;
-    private int DELAY = 20;
+    private int DELAY = 10;
 
-    private static final int[][] offset= {{-1,0},{0,1},{1,0},{0,-1}};
+    private static final int[][] offset = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
     public AlgoVisualizer(String mazeFile) {
         data = new MazeData(mazeFile);
@@ -29,10 +30,43 @@ public class AlgoVisualizer {
         });
     }
 
-    private void run() {
+    //非递归
+    private boolean run() {
+        setData();
+        Stack<Position> pos = new Stack<>();
+        pos.push(new Position(data.getEntranceX(), data.getEntranceY()));
+        data.visited[data.getEntranceX()][data.getEntranceY()] = true;
+        while (!pos.empty()) {
+            Position tmp = pos.pop();
+            data.path[tmp.getX()][tmp.getY()] = true;
+            setData();
+            if (tmp.getX() == data.getExitX() && tmp.getY() == data.getExitY())
+                 break;
+            for (int i = 0; i < 4; i++) {
+                int newX = tmp.getX() + offset[i][0];
+                int newY = tmp.getY() + offset[i][1];
+
+                if (data.inArea(newX,newY) && data.getMaze(newX,newY) == data.ROAD && !data.visited[newX][newY])
+                {
+                    pos.push(new Position(newX,newY));
+                    data.visited[newX][newY] = true;
+
+                }
+            }
+        }
+        setData();
+        System.out.println("the maze has no solution");
+        return false;
+    }
+    // 递归
+    /*private void run() {
 
         setData();
-        go(data.getEntranceX(),data.getEntranceY());
+        if (!go(data.getEntranceX(),data.getEntranceY()))
+        {
+            System.out.println("the maze has no solution");
+            return;
+        }
         setData();
 
     }
@@ -40,7 +74,7 @@ public class AlgoVisualizer {
 
     private boolean  go(int x,int y){
         if (!data.inArea(x,y))
-            throw new IllegalArgumentException("x , y are out of index in maze");
+            throw new IllegalArgumentException("x or y are out of index in maze");
         data.visited[x][y] = true;
         data.path[x][y] = true;
         setData();
@@ -59,13 +93,12 @@ public class AlgoVisualizer {
         setData();
         return false;
 
-    }
+    }*/
 
     private void setData() {
         algoFrame.render(data);
         AlgoVisuHelper.pause(DELAY);
     }
-
 
 
 }
